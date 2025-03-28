8645b54ec14bea857d6aaffbd32816ebc47da44b
LloydKwak <kwaklloyd@gmail.com>
2025-03-28 20:55:28 +0900

    first problem solve

---
+++ chapter01/StringExam.java
@@ -0,0 +1,13 @@
+public class StringExam {
+	public static void main(String[] args) {
+		String str1 = "hello";
+		String str2 = "hello";
+		System.out.println(str1 == str2);
+		System.out.println(str1.equals(str2));
+
+		String str3 = new String("world");
+		String str4 = new String("world");
+		System.out.println(str3 == str4);
+		System.out.println(str3.equals(str4));
+	}
+}
